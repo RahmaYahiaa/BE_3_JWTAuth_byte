077675f2b4d2d03d9aae8849a6015a356a8bc1d9
Rahma Yahia <rahmahamam521@gmail.com>
2026-09-06 10:36:11 +0300

    feat: implement login endpoint with JWT issuance

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 const authService = require('../services/auth.service');
-const { registerSchema } = require('../validators/auth.validator');
+const { registerSchema, loginSchema } = require('../validators/auth.validator');
 const ApiError = require('../utils/ApiError');
 
 const register = async (req, res, next) => {
@@ -18,6 +18,23 @@ const register = async (req, res, next) => {
   }
 };
 
+const login = async (req, res, next) => {
+  try {
+    const { error, value } = loginSchema.validate(req.body);
+    if (error) {
+      throw new ApiError(400, error.details[0].message);
+    }
+    const result = await authService.loginUser(value);
+    res.status(200).json({
+      success: true,
+      data: result,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
 module.exports = {
   register,
+  login,
 };
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User.model');
 const ApiError = require('../utils/ApiError');
 
@@ -24,6 +25,35 @@ const registerUser = async ({ name, email, password }) => {
   };
 };
 
+const loginUser = async ({ email, password }) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new ApiError(401, 'Invalid email or password');
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    throw new ApiError(401, 'Invalid email or password');
+  }
+
+  const token = jwt.sign(
+    { userId: user._id, role: user.role },
+    process.env.JWT_SECRET,
+    { expiresIn: '1d' }
+  );
+
+  return {
+    token,
+    user: {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    },
+  };
+};
+
 module.exports = {
   registerUser,
+  loginUser,
 };
